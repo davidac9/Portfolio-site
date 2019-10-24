368f10c3eb9b4f0210c029ab21cbec05bee91088
David Collett <davida.collett95@gmail.com>
2019-10-23 18:15:43 -0600

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
 import Footer from './Footer'
-import avatar from '../assets/images/avatar.jpg'
+import davidpic from '../assets/images/davidpic.jpg'
 
 class Header extends React.Component {
     render() {
         return (
             <header id="header">
                 <div className="inner">
-                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
-                    <h1><strong>I am Strata</strong>, a super simple<br />
-                    responsive site template freebie<br />
-                    crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
+                    <a href="#" className="image avatar"><img src={davidpic} alt="" /></a>
+                    <h1><strong>I am David Collett</strong>, a Web Developer <br />
+                    looking for opportunities to <br />
+                    become an even better developer!</h1>
                 </div>
                 <Footer />
             </header>
